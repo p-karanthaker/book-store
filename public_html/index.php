e604951cc3e9e2a89421f51e583ae3e8e6c3691b
Karan Thaker <karan.thaker@hotmail.co.uk>
2016-05-14 14:22:49 +0100

    Quick Comments
    Added some comments to separate the page structure.

--- public_html/index.php
+++ public_html/index.php
@@ -1,65 +1,74 @@
+<!-- Begin HTML -->
 <!DOCTYPE html>
 <html lang="en">
-<head>
+  <head>
 
-  <!-- Basic Page Needs
-  –––––––––––––––––––––––––––––––––––––––––––––––––– -->
-  <meta charset="utf-8">
-  <title>Book Store: Get the London Book</title>
-  <meta name="description" content="Aston Book Store Project">
-  <meta name="author" content="Karan Thaker">
+    <!-- Basic Page Needs
+    –––––––––––––––––––––––––––––––––––––––––––––––––– -->
+    <meta charset="utf-8">
+    <title>Book Store: Get the London Book</title>
+    <meta name="description" content="Aston Book Store Project">
+    <meta name="author" content="Karan Thaker">
 
-  <!-- Mobile Specific Metas
-  –––––––––––––––––––––––––––––––––––––––––––––––––– -->
-  <meta name="viewport" content="width=device-width, initial-scale=1">
+    <!-- Mobile Specific Metas
+    –––––––––––––––––––––––––––––––––––––––––––––––––– -->
+    <meta name="viewport" content="width=device-width, initial-scale=1">
 
-  <!-- FONT
-  –––––––––––––––––––––––––––––––––––––––––––––––––– -->
-  <link href="//fonts.googleapis.com/css?family=Raleway:400,300,600" rel="stylesheet" type="text/css">
+    <!-- FONT
+    –––––––––––––––––––––––––––––––––––––––––––––––––– -->
+    <link href="//fonts.googleapis.com/css?family=Raleway:400,300,600" rel="stylesheet" type="text/css">
 
-  <!-- CSS
-  –––––––––––––––––––––––––––––––––––––––––––––––––– -->
-  <link rel="stylesheet" href="css/normalize.css">
-  <link rel="stylesheet" href="css/skeleton.css">
+    <!-- CSS
+    –––––––––––––––––––––––––––––––––––––––––––––––––– -->
+    <link rel="stylesheet" href="css/normalize.css">
+    <link rel="stylesheet" href="css/skeleton.css">
 
-  <!-- Favicon
-  –––––––––––––––––––––––––––––––––––––––––––––––––– -->
-  <link rel="icon" type="image/png" href="img/layout/favicon.ico">
+    <!-- Favicon
+    –––––––––––––––––––––––––––––––––––––––––––––––––– -->
+    <link rel="icon" type="image/png" href="img/layout/favicon.ico">
 
-</head>
-<body>
+  </head>
+  <!-- End Head -->
+  <body>
 
-  <!-- Primary Page Layout
-  –––––––––––––––––––––––––––––––––––––––––––––––––– -->
-  <div class="container">
-    <div class="row">
-      <!-- Title -->
-      <div class="two-thirds column" style="margin-top: 5%">
-        <h1>Book Store</h1>
-      </div>
-      <!-- End Title -->
-      <!-- Log In -->
-      <!-- TODO: If there is a session, then show "Sign Out" and "My Account" -->
-      <div class="one-third column" style="margin-top: 5%">
-        <a class="button button-primary u-full-width" href="login.php">Sign In</a>
-        <a class="button button-primary u-full-width" href="login.php">My Account</a>
-      </div>
-      <!-- End Login -->
-    </div>
-    
-    <div class="row">
-      <div class="twelve columns">
-      <ol class="breadcrumb">
-        <li class="active">Home</li>
-        <li><a href="#">Books</a></li>
-        <li><a href="#">Basket</a></li>
-      </ol>
+    <!-- Primary Page Layout
+    –––––––––––––––––––––––––––––––––––––––––––––––––– -->
+    <div class="container">
+      <div class="row">
+        <!-- Title -->
+        <div class="two-thirds column" style="margin-top: 5%">
+          <h1>Book Store</h1>
         </div>
-    </div>
-    
-  </div>
+        <!-- End Title -->
+        <!-- Log In -->
+        <!-- TODO: If there is a session, then show "Sign Out" and "My Account" -->
+        <div class="one-third column" style="margin-top: 5%">
+          <a class="button button-primary u-full-width" href="login.php">Sign In</a>
+          <a class="button button-primary u-full-width" href="login.php">My Account</a>
+        </div>
+        <!-- End Login -->
+      </div>
 
-<!-- End Document
-  –––––––––––––––––––––––––––––––––––––––––––––––––– -->
-</body>
+      <div class="row">
+        <div class="twelve columns">
+        <ol class="breadcrumb">
+          <li class="active">Home</li>
+          <li><a href="#">Books</a></li>
+          <li><a href="#">Basket</a></li>
+        </ol>
+          </div>
+      </div>
+
+    </div>
+  </body>
+  <!-- End Body -->
 </html>
+<!-- End HTML -->
+
+<!-- Begin PHP -->
+<?php
+
+?>
+<!-- End PHP -->
+
+<!-- End Document -->
--- public_html/register.php
+++ public_html/register.php
@@ -1,70 +1,78 @@
+<!-- Begin HTML-->
 <!DOCTYPE html>
 <html lang="en">
-<head>
+  <head>
+    <!-- Basic Page Needs
+    –––––––––––––––––––––––––––––––––––––––––––––––––– -->
+    <meta charset="utf-8">
+    <title>Registration</title>
+    <meta name="description" content="Registration">
+    <meta name="author" content="Karan Thaker">
 
-  <!-- Basic Page Needs
-  –––––––––––––––––––––––––––––––––––––––––––––––––– -->
-  <meta charset="utf-8">
-  <title>Registration</title>
-  <meta name="description" content="Registration">
-  <meta name="author" content="Karan Thaker">
+    <!-- Mobile Specific Metas
+    –––––––––––––––––––––––––––––––––––––––––––––––––– -->
+    <meta name="viewport" content="width=device-width, initial-scale=1">
 
-  <!-- Mobile Specific Metas
-  –––––––––––––––––––––––––––––––––––––––––––––––––– -->
-  <meta name="viewport" content="width=device-width, initial-scale=1">
+    <!-- FONT
+    –––––––––––––––––––––––––––––––––––––––––––––––––– -->
+    <link href="//fonts.googleapis.com/css?family=Raleway:400,300,600" rel="stylesheet" type="text/css">
 
-  <!-- FONT
-  –––––––––––––––––––––––––––––––––––––––––––––––––– -->
-  <link href="//fonts.googleapis.com/css?family=Raleway:400,300,600" rel="stylesheet" type="text/css">
+    <!-- CSS
+    –––––––––––––––––––––––––––––––––––––––––––––––––– -->
+    <link rel="stylesheet" href="css/normalize.css">
+    <link rel="stylesheet" href="css/skeleton.css">
 
-  <!-- CSS
-  –––––––––––––––––––––––––––––––––––––––––––––––––– -->
-  <link rel="stylesheet" href="css/normalize.css">
-  <link rel="stylesheet" href="css/skeleton.css">
+    <!-- Favicon
+    –––––––––––––––––––––––––––––––––––––––––––––––––– -->
+    <link rel="icon" type="image/png" href="img/layout/favicon.ico">
 
-  <!-- Favicon
-  –––––––––––––––––––––––––––––––––––––––––––––––––– -->
-  <link rel="icon" type="image/png" href="img/layout/favicon.ico">
+  </head>
+  <!-- End Head -->
+  <body>
 
-</head>
-<body>
-
-  <!-- Primary Page Layout
-  –––––––––––––––––––––––––––––––––––––––––––––––––– -->
-  <div class="container">  
-    <!-- Title -->
-      <div class="row">
-        <div class="twelve columns" style="margin-top: 5%">
-          <h2 class="u-text-centre">Register</h2>
+    <!-- Primary Page Layout
+    –––––––––––––––––––––––––––––––––––––––––––––––––– -->
+    <div class="container">  
+      <!-- Title -->
+        <div class="row">
+          <div class="twelve columns" style="margin-top: 5%">
+            <h2 class="u-text-centre">Register</h2>
+          </div>
         </div>
-      </div>
-      <!-- End Title -->
-    
-      <!-- Login Form -->
-      <div id="loginForm" class="row">
-        <div class="four columns offset-by-one-third">
-          <form id="register" action="register.php" method="post">
-          <label for="username">Username</label>
-          <input class="u-full-width" type="text" placeholder="Username" id="username" name="username" required>
+        <!-- End Title -->
 
-          <label for="password">Password</label>
-          <input class="u-full-width" type="password" placeholder="Password" id="password" name="password" required>
-          <input class="u-full-width" type="password" placeholder="Confirm" id="passwordConfirm" name="passwordConfirm" required>
+        <!-- Login Form -->
+        <div id="loginForm" class="row">
+          <div class="four columns offset-by-one-third">
+            <form id="register" action="register.php" method="post">
+            <label for="username">Username</label>
+            <input class="u-full-width" type="text" placeholder="Username" id="username" name="username" required>
 
-          <label for="userType">User Type</label>
-          <select class="u-full-width" id="userType" required>
-            <option value="Option 1">Student</option>
-            <option value="Option 2">Staff</option>
-          </select>
-          <input class="button-primary u-full-width" type="submit" name="submit" value="Register">
-          <a class="button u-full-width" href="index.php">Cancel</a>
-        </form>
+            <label for="password">Password</label>
+            <input class="u-full-width" type="password" placeholder="Password" id="password" name="password" required>
+            <input class="u-full-width" type="password" placeholder="Confirm" id="passwordConfirm" name="passwordConfirm" required>
+
+            <label for="userType">User Type</label>
+            <select class="u-full-width" id="userType" required>
+              <option value="Option 1">Student</option>
+              <option value="Option 2">Staff</option>
+            </select>
+            <input class="button-primary u-full-width" type="submit" name="submit" value="Register">
+            <a class="button u-full-width" href="index.php">Cancel</a>
+          </form>
+          </div>
         </div>
-      </div>
-      <!-- End Login Form -->
-  </div>
-
-<!-- End Document
-  –––––––––––––––––––––––––––––––––––––––––––––––––– -->
-</body>
+        <!-- End Login Form -->
+    </div>
+  </body>
+  <!-- End Body-->
 </html>
+<!-- End HTML -->
+
+<!-- Begin PHP -->
+<?php
+
+?>
+<!-- End PHP -->
+
+<!-- End Document -->
