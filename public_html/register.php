e90fe59793f41aff1a62b371753d63063b5ce588
Karan Thaker <p-karanthaker@users.noreply.github.com>
2016-05-14 18:58:34 +0100

    Dev (#9)
    * Changes to all php/html files. (#8)
    
    Quick change to add pages for login and register.
    
    * Quick Comments
    
    Added some comments to separate the page structure.
    
    * Added some php
    
    Testing getting post form values in php.
    
    * Beginning to add more registration code.
    
    Currently adding registration validation...tbc
    
    * Validation
    
    Added more validation for registration form.

---
+++ public_html/register.php
@@ -0,0 +1,150 @@
+<!-- Begin HTML-->
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <!-- Basic Page Needs
+    –––––––––––––––––––––––––––––––––––––––––––––––––– -->
+    <meta charset="utf-8">
+    <title>Registration</title>
+    <meta name="description" content="Registration">
+    <meta name="author" content="Karan Thaker">
+
+    <!-- Mobile Specific Metas
+    –––––––––––––––––––––––––––––––––––––––––––––––––– -->
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+
+    <!-- FONT
+    –––––––––––––––––––––––––––––––––––––––––––––––––– -->
+    <link href="//fonts.googleapis.com/css?family=Raleway:400,300,600" rel="stylesheet" type="text/css">
+
+    <!-- CSS
+    –––––––––––––––––––––––––––––––––––––––––––––––––– -->
+    <link rel="stylesheet" href="css/normalize.css">
+    <link rel="stylesheet" href="css/skeleton.css">
+
+    <!-- Favicon
+    –––––––––––––––––––––––––––––––––––––––––––––––––– -->
+    <link rel="icon" type="image/png" href="img/layout/favicon.ico">
+
+  </head>
+  <!-- End Head -->
+  <body>
+
+    <!-- Primary Page Layout
+    –––––––––––––––––––––––––––––––––––––––––––––––––– -->
+    <div class="container">  
+      <!-- Title -->
+        <div class="row">
+          <div class="twelve columns" style="margin-top: 5%">
+            <h2 class="u-text-centre">Register</h2>
+          </div>
+        </div>
+        <!-- End Title -->
+
+        <!-- Registration Form -->
+        <div class="row">
+          <div class="four columns offset-by-one-third">
+            <form name="registerform" action="register.php" method="post">
+              <label for="username">Username</label>
+              <input class="u-full-width" type="text" placeholder="Username" name="username" required>
+
+              <label for="password">Password</label>
+              <input class="u-full-width" type="password" placeholder="Password" name="password" required>
+              <input class="u-full-width" type="password" placeholder="Confirm" name="password_confirm" required>
+
+              <label for="user_type">User Type</label>
+              <select class="u-full-width" name="user_type" required>
+                <option value="Student">Student</option>
+                <option value="Staff">Staff</option>
+              </select>
+              <input class="button-primary u-full-width" type="submit" name="register" value="Register">
+              <a class="button u-full-width" href="index.php">Cancel</a>
+          </form>
+          </div>
+        </div>
+        <!-- End Login Form -->
+    </div>
+  </body>
+  <!-- End Body-->
+</html>
+<!-- End HTML -->
+
+<!-- Begin PHP -->
+<?php
+  class AccountRegistration
+  {
+    public function __construct()
+    {
+      if(isset($_POST["register"]))
+      {
+        $this->registerUser();
+      }
+    }
+    
+    private function registerUser()
+    {
+      
+        if ($this->validateFormData())
+        {
+          echo "Valid!";
+        } else 
+        {
+          echo "Something went wrong.";
+          return false;
+        }
+    } 
+    
+    /**
+     * Validates the user input on the registration form
+     * @return bool Returns whether the form is valid or not
+     */    
+    private function validateFormData()
+    {
+      define("MIN_LENGTH", 6);
+      define("MAX_LENGTH", 12);
+      define("REGEX_MATCHER", '/^[a-z0-9]{6,12}$/i');
+      
+      if(!empty($_POST["username"])
+        && !empty($_POST["password"])
+        && !empty($_POST["password_confirm"])
+        && !empty($_POST["user_type"])
+        && strlen($_POST["username"]) <= MAX_LENGTH
+        && strlen($_POST["username"]) >= MIN_LENGTH
+        && preg_match(REGEX_MATCHER, $_POST["username"])
+        && strlen($_POST["password"]) <= MAX_LENGTH
+        && strlen($_POST["password"]) >= MIN_LENGTH
+        && preg_match(REGEX_MATCHER, $_POST["password"])
+        && ($_POST["password"] === $_POST["password_confirm"])
+        && ($_POST["user_type"] === "Student") || ($_POST["user_type"] === "Staff"))
+      {
+        echo $_POST["username"];
+        return true;
+      } else {
+        return false;
+      }
+    }
+    
+    function random()
+    {
+      if(isset($_POST["register"])) 
+      {
+        $salt = bin2hex(openssl_random_pseudo_bytes(4));
+        echo $salt, "<br>";
+        $username = $_POST["username"];
+        $password_hash = crypt($_POST["password"], $salt);
+        $password_confirm_hash = crypt($_POST["password_confirm"], $salt);
+        $user_type = $_POST["user_type"];
+        echo $username, "<br>", $password_hash, "<br>", $password_confirm_hash, "<br>", $user_type, "<br>";
+
+      } else 
+      {
+        return false;
+      }
+    }
+    
+  }
+  $application = new AccountRegistration();
+?>
+<!-- End PHP -->
+
+<!-- End Document -->
