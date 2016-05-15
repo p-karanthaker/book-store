76c730284e530417bc78ed37f675ce1703c1d727
Karan Thaker <karan.thaker@hotmail.co.uk>
2016-05-15 14:49:19 +0100

    Changed SQL
    Changed some number values to not null and default to 0.

--- resources/db/book-store.sql
+++ resources/db/book-store.sql
@@ -7,40 +7,40 @@
 CREATE TABLE user 
 (
 	user_id INT NOT NULL AUTO_INCREMENT PRIMARY KEY, 
-    username VARCHAR(12), 
-    password_hash CHAR(60),
-    password_salt CHAR(44),
-    type ENUM('STUDENT', 'STAFF'), 
-    balance DECIMAL(13,2)
+  username VARCHAR(12), 
+  password_hash CHAR(60),
+  password_salt CHAR(44),
+  type ENUM('STUDENT', 'STAFF'), 
+  balance DECIMAL(13,2) NOT NULL DEFAULT '0.00';
 );
 
 CREATE TABLE category
 (
 	cat_id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
-    name VARCHAR(50)
+  name VARCHAR(50)
 );
 
 CREATE TABLE books
 (
 	book_id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
-    title VARCHAR(100),
-    authors VARCHAR(100),
-    cat_id INT NOT NULL,
-    FOREIGN KEY(cat_id) REFERENCES category(cat_id),
-    quantity INT,
-    price DECIMAL(13,2),
-    description VARCHAR(500),
-    photo VARCHAR(100)
+  title VARCHAR(100),
+  authors VARCHAR(100),
+  cat_id INT NOT NULL,
+  FOREIGN KEY(cat_id) REFERENCES category(cat_id),
+  quantity INT NOT NULL DEFAULT '0',
+  price DECIMAL(13,2) NOT NULL DEFAULT '0.00',
+  description VARCHAR(500),
+  photo VARCHAR(100)
 );
 
 CREATE TABLE basket
 (
 	order_id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
-    user_id INT NOT NULL,
-    book_id INT NOT NULL,
-    FOREIGN KEY(user_id) REFERENCES user(user_id),
-    FOREIGN KEY(book_id) REFERENCES books(book_id),
-    quantity INT,
-    cost DECIMAL(13,2),
-    date DATETIME
+  user_id INT NOT NULL,
+  book_id INT NOT NULL,
+  FOREIGN KEY(user_id) REFERENCES user(user_id),
+  FOREIGN KEY(book_id) REFERENCES books(book_id),
+  quantity INT NOT NULL DEFAULT '0',
+  cost DECIMAL(13,2) NOT NULL DEFAULT '0.00',
+  date DATETIME
 );
