b7a97c13c367c3ecb88e085fa552ed5a53b3450e
Karan Thaker <karan.thaker@hotmail.co.uk>
2016-06-19 13:59:58 +0100

    Previous check in didn't work properly

--- public_html/resources/db/book-store.sql
+++ public_html/resources/db/book-store.sql
@@ -31,22 +31,48 @@ CREATE TABLE Books
 	photo VARCHAR(500)
 );
 
-CREATE TABLE Basket
-(
-	order_id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
-	user_id INT NOT NULL,
-	book_id INT NOT NULL,
-	FOREIGN KEY(user_id) REFERENCES User(user_id),
-	FOREIGN KEY(book_id) REFERENCES Books(book_id),
-	quantity INT NOT NULL DEFAULT '0',
-	cost DECIMAL(13,2) NOT NULL DEFAULT '0.00',
-	date DATETIME
-);
-
 CREATE TABLE BookCategory
 (
 	book_id INT NOT NULL,
 	cat_id INT NOT NULL,
 	FOREIGN KEY(book_id) REFERENCES Books(book_id),
 	FOREIGN KEY(cat_id) REFERENCES Category(cat_id)
+);
+
+CREATE TABLE Basket
+(
+	basket_id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+	user_id INT NOT NULL,
+	FOREIGN KEY(user_id) REFERENCES User(user_id)
+);
+
+CREATE TABLE BasketItem
+(
+	basket_id INT NOT NULL,
+	book_id INT NOT NULL,
+	FOREIGN KEY(basket_id) REFERENCES Basket(basket_id),
+	FOREIGN KEY(book_id) REFERENCES Books(book_id),
+	quantity INT NOT NULL DEFAULT '0',
+	cost DECIMAL(13,2) NOT NULL DEFAULT '0.00'
+);
+
+CREATE TABLE Orders
+(
+	order_id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+	user_id INT NOT NULL,
+	FOREIGN KEY(user_id) REFERENCES User(user_id),
+	active BOOLEAN NOT NULL DEFAULT true,
+	date DATETIME
+);
+
+CREATE TABLE OrderItem
+(
+	order_id INT NOT NULL,
+	book_id INT NOT NULL,
+	quantity INT NOT NULL,
+	cost DECIMAL(13,2) NOT NULL,
+	FOREIGN KEY(order_id) REFERENCES Orders(order_id),
+	FOREIGN KEY(book_id) REFERENCES Books(book_id),
+	FOREIGN KEY(quantity) REFERENCES BasketItem(quantity),
+	FOREIGN KEY(cost) REFERENCES BasketItem(cost)
 );
