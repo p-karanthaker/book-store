5afea155edc50dabe47f76ddb39d7613135c0585
Karan Thaker <karan.thaker@hotmail.co.uk>
2016-06-19 13:50:54 +0100

    New database design
    Changed basket, order, and book category design

--- public_html/resources/db/book-store.sql
+++ public_html/resources/db/book-store.sql
@@ -4,43 +4,49 @@
 -- basket (orderID, userID, bookID, quantity, cost, date)
 -- category (catID, name)
 
-CREATE TABLE user 
+CREATE TABLE User 
 (
 	user_id INT NOT NULL AUTO_INCREMENT PRIMARY KEY, 
-  username VARCHAR(12), 
-  password_hash CHAR(60),
-  password_salt CHAR(44),
-  type ENUM('STUDENT', 'STAFF'), 
-  balance DECIMAL(13,2) NOT NULL DEFAULT '0.00';
+	username VARCHAR(12), 
+	password_hash CHAR(60),
+	password_salt CHAR(44),
+	type ENUM('STUDENT', 'STAFF'), 
+	balance DECIMAL(13,2) NOT NULL DEFAULT '0.00'
 );
 
-CREATE TABLE category
+CREATE TABLE Category
 (
 	cat_id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
-  name VARCHAR(50)
+	name VARCHAR(50)
 );
 
-CREATE TABLE books
+CREATE TABLE Books
 (
 	book_id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
-  title VARCHAR(100),
-  authors VARCHAR(100),
-  cat_id INT NOT NULL,
-  FOREIGN KEY(cat_id) REFERENCES category(cat_id),
-  quantity INT NOT NULL DEFAULT '0',
-  price DECIMAL(13,2) NOT NULL DEFAULT '0.00',
-  description VARCHAR(500),
-  photo VARCHAR(100)
+	title VARCHAR(100),
+	authors VARCHAR(100),
+	quantity INT NOT NULL DEFAULT '0',
+	price DECIMAL(13,2) NOT NULL DEFAULT '0.00',
+	description VARCHAR(500),
+	photo VARCHAR(500)
 );
 
-CREATE TABLE basket
+CREATE TABLE Basket
 (
 	order_id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
-  user_id INT NOT NULL,
-  book_id INT NOT NULL,
-  FOREIGN KEY(user_id) REFERENCES user(user_id),
-  FOREIGN KEY(book_id) REFERENCES books(book_id),
-  quantity INT NOT NULL DEFAULT '0',
-  cost DECIMAL(13,2) NOT NULL DEFAULT '0.00',
-  date DATETIME
+	user_id INT NOT NULL,
+	book_id INT NOT NULL,
+	FOREIGN KEY(user_id) REFERENCES User(user_id),
+	FOREIGN KEY(book_id) REFERENCES Books(book_id),
+	quantity INT NOT NULL DEFAULT '0',
+	cost DECIMAL(13,2) NOT NULL DEFAULT '0.00',
+	date DATETIME
+);
+
+CREATE TABLE BookCategory
+(
+	book_id INT NOT NULL,
+	cat_id INT NOT NULL,
+	FOREIGN KEY(book_id) REFERENCES Books(book_id),
+	FOREIGN KEY(cat_id) REFERENCES Category(cat_id)
 );
