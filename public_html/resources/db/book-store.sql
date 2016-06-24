c7a3f6ee6407eaa41ccb79c20bde74286933090c
Karan Thaker <karan.thaker@capgemini.com>
2016-06-24 10:34:26 +0100

    Changes so script works on production server

--- public_html/resources/db/book-store.sql
+++ public_html/resources/db/book-store.sql
@@ -1,72 +1,72 @@
 -- Tables to create in DB
-CREATE TABLE User 
+CREATE TABLE `user` 
 (
 	user_id INT NOT NULL AUTO_INCREMENT PRIMARY KEY, 
 	username VARCHAR(12), 
 	password_hash CHAR(60),
 	password_salt CHAR(44),
-	type ENUM('STUDENT', 'STAFF'), 
+	`type` ENUM('STUDENT', 'STAFF'), 
 	balance DECIMAL(13,2) NOT NULL DEFAULT '0.00'
 ) ENGINE = InnoDB;
 
-CREATE TABLE Category
+CREATE TABLE category
 (
 	cat_id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
-	name VARCHAR(50)
+	`name` VARCHAR(50)
 ) ENGINE = InnoDB;
 
-CREATE TABLE Books
+CREATE TABLE books
 (
 	book_id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
 	title VARCHAR(100),
-	authors VARCHAR(100),
+	`authors` VARCHAR(100),
 	quantity INT NOT NULL DEFAULT '0',
 	price DECIMAL(13,2) NOT NULL DEFAULT '0.00',
 	description VARCHAR(5000),
 	photo VARCHAR(500)
 ) ENGINE = InnoDB;
 
-CREATE TABLE BookCategory
+CREATE TABLE bookcategory
 (
 	book_id INT NOT NULL,
 	cat_id INT NOT NULL,
-	FOREIGN KEY(book_id) REFERENCES Books(book_id),
-	FOREIGN KEY(cat_id) REFERENCES Category(cat_id)
+	FOREIGN KEY(book_id) REFERENCES books(book_id),
+	FOREIGN KEY(cat_id) REFERENCES category(cat_id)
 ) ENGINE = InnoDB;
 
-CREATE TABLE Basket
+CREATE TABLE basket
 (
 	basket_id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
 	user_id INT NOT NULL,
-	FOREIGN KEY(user_id) REFERENCES User(user_id),
+	FOREIGN KEY(user_id) REFERENCES `user`(user_id),
 	UNIQUE KEY `user_id_UNIQUE` (user_id)
 ) ENGINE = InnoDB;
 
-CREATE TABLE BasketItem
+CREATE TABLE basketitem
 (
 	basket_id INT NOT NULL,
 	book_id INT NOT NULL,
-	FOREIGN KEY(basket_id) REFERENCES Basket(basket_id),
-	FOREIGN KEY(book_id) REFERENCES Books(book_id),
+	FOREIGN KEY(basket_id) REFERENCES basket(basket_id),
+	FOREIGN KEY(book_id) REFERENCES books(book_id),
 	quantity INT NOT NULL DEFAULT '0',
 	cost DECIMAL(13,2) NOT NULL DEFAULT '0.00'
 ) ENGINE = InnoDB;
 
-CREATE TABLE Orders
+CREATE TABLE orders
 (
 	order_id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
 	user_id INT NOT NULL,
-	FOREIGN KEY(user_id) REFERENCES User(user_id),
+	FOREIGN KEY(user_id) REFERENCES `user`(user_id),
 	active BOOLEAN NOT NULL DEFAULT true,
-	date DATETIME
+	`date` DATETIME
 ) ENGINE = InnoDB;
 
-CREATE TABLE OrderItem
+CREATE TABLE orderitem
 (
 	order_id INT NOT NULL,
 	book_id INT NOT NULL,
 	quantity INT NOT NULL,
 	cost DECIMAL(13,2) NOT NULL,
-	FOREIGN KEY(order_id) REFERENCES Orders(order_id),
-	FOREIGN KEY(book_id) REFERENCES Books(book_id)
+	FOREIGN KEY(order_id) REFERENCES orders(order_id),
+	FOREIGN KEY(book_id) REFERENCES books(book_id)
 ) ENGINE = InnoDB;
