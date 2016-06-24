3ef7e27ff5da58e41a4352f9e1e799c393c6c2ba
Karan Thaker <karan.thaker@hotmail.co.uk>
2016-06-24 21:30:50 +0100

    Changed Bootstrap and jQuery links
    Now  fetch from CDN rather than locally to improve page load.

--- public_html/shop.php
+++ public_html/shop.php
@@ -40,8 +40,8 @@
 
     <!-- JavaScript
     –––––––––––––––––––––––––––––––––––––––––––––––––– -->
-    <script src="/js/jquery.min.js"></script>
-    <script src="/js/bootstrap.min.js"></script>
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.2/jquery.min.js"></script>
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/js/bootstrap.min.js"></script>
     <script src="/js/shop.js"></script>
     
     <!-- Favicon
