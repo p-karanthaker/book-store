3dd23680d60f7b636c524ebfd75385741d4e33f7
Karan Thaker <karan.thaker@hotmail.co.uk>
2016-07-10 20:55:22 +0100

    Order for scripts, report, and regex restriction

--- public_html/resources/php/Books.php
+++ public_html/resources/php/Books.php
@@ -229,7 +229,7 @@
       $quantity = array_key_exists("quantity", $book) ? $book["quantity"] : "";
       $price = array_key_exists("price", $book) ? $book["price"] : "";
 
-      $stringmatcher="a-z0-9\s,.+*?$!:\-'\"\&";
+      $stringmatcher="a-z0-9\s,.?!:\-'\"\&";
       define("REGEX_MATCHER_ONE", "/^[$stringmatcher]{1,100}$/i");
       define("REGEX_MATCHER_FIF", "/^[$stringmatcher]{1,50}$/i");
       define("REGEX_MATCHER_NUM", "/^[\d]{1,11}$/");
